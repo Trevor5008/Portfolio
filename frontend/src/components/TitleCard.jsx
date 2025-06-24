909f36756a4163e003c8d6858de1dc2c1e44da5f
Trevor5008 <trezrevner@icloud.com>
2025-06-23 20:31:44 -0400

    Added rock-salt custom font for title card

--- frontend/src/components/TitleCard.jsx
+++ frontend/src/components/TitleCard.jsx
@@ -1,13 +1,15 @@
 const TitleCard = () => {
   return (
     <div id="titleCard" 
-      className="absolute inset-0 flex items-center justify-center border text-cyan-400">
-      <div className="text-center">
-        <h1>Trevor Werner</h1>
-        <button className="text-cyan-400 border-2 padding-2 w-16 mt-6 rounded">Enter</button>
+      className="absolute inset-0 flex items-center justify-center border border-cyan-400 text-cyan-400">
+      <div className="text-center p-8">
+        <h1 className="font-rock-salt text-2xl">Trevor Werner</h1>
+        <button className="font-rock-salt text-xs text-cyan-400 border-2 p-2 w-20 mt-10 rounded">Enter</button>
       </div>
     </div>
   )
 }
 
 export default TitleCard;
+
+
